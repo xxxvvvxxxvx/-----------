36f75cb2d292f1c3f8f8a4b300c46a7f4f67918a
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2022-01-10 14:37:09 +0000

    Initial commit

---
+++ datbaze/aaa.py
@@ -0,0 +1,9 @@
+from replit import db
+
+
+db['Anna']='24957474'
+print(db['Anna'])
+
+d={}
+d['Anna']='24957474'
+print(d['Anna'])
---
+++ main.py
@@ -0,0 +1,11 @@
+import d_gramatinnna
+
+from replit import db
+
+def piev_kontaktu(vards, tel_nr):
+  if vards in db:
+    print('Kontakts ji pievienots')
+  else:
+    db[vards]=tel_nr
+    print(f'Pievienots:{vards}:{tel_nr}')
+    
